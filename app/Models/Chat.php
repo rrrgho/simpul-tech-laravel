361f932c37446b9a14cbb7f977c6399c83d37505
igho <rianiregho@gmail.com>
2023-11-24 05:15:56 +0700

    FEAT: add username of chat

--- app/Models/Chat.php
+++ app/Models/Chat.php
@@ -10,4 +10,15 @@ class Chat extends Model
     use HasFactory;
     protected $table = 'group_chats';
     protected $guarded = [];
+
+    protected $appends = ['name'];
+    protected $hidden = ['user'];
+
+    public function user(){
+        return $this->belongsTo(User::class, 'user_id', 'id');
+    }
+
+    public function getNameAttribute(){
+        return $this->user->name;
+    }
 }
